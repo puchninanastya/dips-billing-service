c50c73dd661bd9269416256d29975541be109f5e
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-15 02:38:55 +0300

    Payment model added.

--- billing_service/models.py
+++ billing_service/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Payment(models.Model):
+    user_id = models.IntegerField(
+        blank=False,
+        null=False,
+        verbose_name="By user")
+    order_id = models.IntegerField(
+        blank=False,
+        null=False,
+        verbose_name="For order")
+    payment_date = models.DateField(
+        blank=True,
+        null=False,
+        default=now,
+        verbose_name="Payment date")
+    amount_paid = models.PositiveIntegerField(
+        verbose_name="Amount paid")
+    payment_method = models.CharField(
+        max_length=2,
+        blank=True,
+        null=False,
+        default='CH',
+        choices=(
+            ('CH', 'Cash payment'),
+            ('CD', 'Payment by card')),
+        verbose_name="Payment method")
+    account_number = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        verbose_name="Account number")
+
+    class Meta:
+        verbose_name = "Payment"
+        verbose_name_plural = "Payments"
+
+    def __str__(self):
+        return "id {}".format(self.pk)
